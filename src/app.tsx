ee81c2970d7d6f2ccfc936a0f09c0f9675edf2d9
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-05-11 07:03:50 +0700

    feat: to-do app

--- src/app.tsx
+++ src/app.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const tasks = [
+  {
+    id: 1,
+    title: "Breakfast",
+    completed: true,
+    date: new Date("2025-05-11 7:00"),
+  },
+  {
+    id: 2,
+    title: "Cleaning The Room",
+    completed: false,
+    date: new Date("2025-05-11 08:00"),
+  },
+];
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Doo Now</h1>
+      <ul>
+        {tasks.map((task) => {
+          return (
+            <li>
+              <h2>{task.title}</h2>
+              <p>{task.completed}</p>
+              <p>Date Time: {task.date.toLocaleString()}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
-
-export default App

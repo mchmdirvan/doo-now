c9c89fffe04795996824bb0bec26fb4d44c3cbfe
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-05-21 20:17:24 +0700

    style: set text button to xs

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,5 +1,7 @@
 export default function Button({ children }: { children: React.ReactNode }) {
   return (
-    <button className="bg-red-500 rounded-full px-2 py-1">{children}</button>
+    <button className="bg-red-500 rounded-full px-2 py-1 text-xs">
+      {children}
+    </button>
   );
 }

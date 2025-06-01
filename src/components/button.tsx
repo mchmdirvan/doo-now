f9d1f15946c49e9507b0c7f5413cd8fc333e38b9
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-06-01 16:12:51 +0700

    refactor: button by using props from ts

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,16 +1,5 @@
-export default function Button({
-  onClick,
-  children,
-}: {
-  onClick: () => void;
-  children: React.ReactNode;
-}) {
-  return (
-    <button
-      onClick={onClick}
-      className="bg-red-500 max-h-8 px-2 rounded-sm text-xs"
-    >
-      {children}
-    </button>
-  );
+export default function Button(
+  props: React.ButtonHTMLAttributes<HTMLButtonElement>
+) {
+  return <button {...props} />;
 }

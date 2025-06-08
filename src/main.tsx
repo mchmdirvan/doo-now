edb99dd375692fc2592f5eefd63664ea0ee36213
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-06-08 14:08:32 +0700

    feat: add navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Link, Route, Routes } from "react-router";
 import { AboutRoute } from "./routes/about";
 import { HomeRoute } from "./routes/home";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
+    <nav className="bg-red-100 p-4 text-sm">
+      <ul className="flex gap-4">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
     <Routes>
       <Route path="/" element={<HomeRoute />}></Route>
       <Route path="/about" element={<AboutRoute />} />

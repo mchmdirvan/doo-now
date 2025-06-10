00f89e106a505728c8208e39e896539942e49fc9
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-06-11 02:44:03 +0700

    chore: init

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,10 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { BrowserRouter, Link, Route, Routes } from "react-router";
-import { HomeRoute } from "./routes/home";
-import { AboutRoute } from "./routes/about";
-import TaskDetailRoute from "./routes/task-detail";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-createRoot(document.getElementById("root")!).render(<App />);
-
-function App() {
-  return (
-    <BrowserRouter>
-      <nav className="bg-blue-100 p-4 text-sm">
-        <ul className="flex gap-4">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<HomeRoute />} />
-        <Route path="/about" element={<AboutRoute />} />
-        <Route path="/tasks/:taskId" element={<TaskDetailRoute />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
--- src/modules/type.ts
+++
@@ -1,6 +0,0 @@
-export type Task = {
-  id: number;
-  title: string;
-  completed: boolean;
-  date: Date;
-};

1d04325d5d6b711f32289c7b3d5cb4dc6e87a676
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-06-14 18:00:16 +0700

    feat: add button view, edit, delete, on card

--- src/components/task-card.tsx
+++ src/components/task-card.tsx
@@ -1,7 +1,8 @@
-import { Calendar } from "lucide-react";
+import { Calendar, Eye, PencilIcon, Trash2Icon } from "lucide-react";
 
 import { type Task } from "../types/task";
 import { Checkbox } from "./ui/checkbox";
+import { Button } from "./ui/button";
 
 type TaskProps = {
   task: Task;
@@ -9,20 +10,34 @@ type TaskProps = {
 
 export function TaskCard({ task }: TaskProps) {
   return (
-    <li className="flex gap-3 border-b pb-3">
-      <div>
-        <Checkbox id="tasks" />
+    <li className="flex justify-between border-b pb-3">
+      <div className="flex gap-3">
+        <div>
+          <Checkbox id="tasks" />
+        </div>
+
+        <div className="space-y-1">
+          <p>{task.title}</p>
+          <p className="text-xs text-neutral-500">{task.description}</p>
+          <p className="flex gap-1 text-xs text-neutral-500">
+            <span>
+              <Calendar size={12} />
+            </span>
+            {task.dueDate.toLocaleString()}
+          </p>
+        </div>
       </div>
 
-      <div className="space-y-1">
-        <p>{task.title}</p>
-        <p className="text-xs text-neutral-500">{task.description}</p>
-        <p className="flex gap-1 text-xs text-neutral-500">
-          <span>
-            <Calendar size={12} />
-          </span>
-          {task.dueDate.toLocaleString()}
-        </p>
+      <div className="space-x-2">
+        <Button variant="secondary" size="icon" className="size-8">
+          <Eye />
+        </Button>
+        <Button variant="secondary" size="icon" className="size-8">
+          <PencilIcon />
+        </Button>
+        <Button variant="destructive" size="icon" className="size-8">
+          <Trash2Icon />
+        </Button>
       </div>
     </li>
   );

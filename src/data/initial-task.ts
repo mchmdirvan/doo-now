1cbc5f9a61d543649af1bf4214cda52c80a3ddca
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-06-14 17:28:18 +0700

    feat: created initial task

---
+++ src/data/initial-task.ts
@@ -0,0 +1,19 @@
+import type { Task } from "@/types/task";
+
+export const initialialTask: Task[] = [
+  {
+    id: 1,
+    title: "Welcome to your task manager! 👋",
+    description:
+      "This is your first task. Click on it to see task details and mark it as complete when you're ready.",
+    isCompleted: false,
+    dueDate: new Date("2025-06-14"),
+  },
+  {
+    id: 2,
+    title: "Add your first real task",
+    description: "Try creating a new task by clicking the 'Add Task' button.",
+    isCompleted: false,
+    dueDate: new Date("2025-06-15"),
+  },
+];

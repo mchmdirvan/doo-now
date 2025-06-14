9d34629f3a5bc077b146a347ce20003f635ea05c
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-06-14 13:27:41 +0700

    feat: created main pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,21 @@
 import { Layout } from "@/components/layouts/layout";
+import { TaskCard } from "@/components/task-card";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
 
 export function Index() {
-  return <Layout>Hello</Layout>;
+  return (
+    <Layout>
+      <section className="mx-auto max-w-3xl py-10">
+        <h1 className="pb-5 text-2xl font-bold">Tasks</h1>
+
+        <TaskCard />
+
+        <Button variant="ghost" size="sm" className="mt-2">
+          <Plus />
+          Add Task
+        </Button>
+      </section>
+    </Layout>
+  );
 }

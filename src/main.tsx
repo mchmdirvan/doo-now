c36f6356fabe630916f78d3e4658c3cb96e50073
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-06-15 11:06:35 +0700

    feat: add pages shadcn form playground

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 // import { Index } from "./pages";
-import { RhfPlayground } from "./pages/rhf-playground";
+// import { RhfPlayground } from "./pages/rhf-playground";
+import { ShadcnFormPlayground } from "./pages/shadcn-form-playground";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* <Index /> */}
-    <RhfPlayground />
+    {/* <RhfPlayground /> */}
+    <ShadcnFormPlayground />
   </StrictMode>,
 );

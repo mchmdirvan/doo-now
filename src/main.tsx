e0ddeac42ae7f70e4e4c145d08f912252901e9cd
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-06-15 10:19:36 +0700

    feat: add pages rhf playground

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { Index } from "./pages";
+// import { Index } from "./pages";
+import { RhfPlayground } from "./pages/rhf-playground";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Index />
-  </StrictMode>
+    {/* <Index /> */}
+    <RhfPlayground />
+  </StrictMode>,
 );
